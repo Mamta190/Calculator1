7e5565d14eb5fcb188211c891b8c0c5848d705e6
Mamta190 <64964289+Mamta190@users.noreply.github.com>
2022-01-11 10:19:22 +0530

    Some changes added
    After some changes

---
+++ script.js
@@ -0,0 +1,15 @@
+function addChar(input, character) {
+	if(input.value == null || input.value == "0")
+		input.value = character
+	else
+		input.value += character
+}
+console.log(Math.min(2,3,1));
+
+console.log(Math.max(-2, -3, -1));
+
+const array1 = [2, 3, 1];
+
+console.log(Math.min(array1));
+
+
